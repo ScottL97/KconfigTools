f8f6d9f392e35af50d947891751cc933ccf1c515
Scott <liuxinhao1997@foxmail.com>
2019-10-02 14:17:50 +0800

    Modify the compareconfig tool to find the lines which are included only by one file, not only the different lines.

--- compareconfig.sh
+++ compareconfig.sh
@@ -1,6 +1,67 @@
 #!/bin/bash
 # compareconfig.sh
 
+find_include_null() {
+cat $1 | grep -v ^# | while read line
+do
+	key=$(echo $line | awk -F = '{print $1}')
+	if [ -n "$key" ]
+	then
+		value1=$(echo $line | awk -F = '{print $2}')
+		# 查找$2中对应行
+		./grepi -q -p "^$key=" $2
+		if [ $? -eq 0 ]
+		then
+			# 获取$2中对应行的值
+			value2=$(grep "^$key=" $2 | awk -F = '{print $2}')
+			if [ $common = y ]
+			then
+				if [ "$value1" = "$value2" ]
+				then
+					((sum++))
+					echo "[$key IN COMMON]  $value1 $sum"
+				fi
+			else
+				if [ "$value1" != "$value2" ]
+				then
+					((sum++))
+					echo "[$key]  $value1 $value2 $sum"
+				fi
+			fi
+		else # 查找$1中存在、$2中不存在的配置项
+			if [ $common = n ]
+			then
+				((sum++))
+				echo "[$key]  $value1 <NULL> $sum"
+			fi
+		fi
+	fi
+done
+if [ $common = n ]
+then
+	# 查找$2中存在、$1中不存在的配置项
+	find_null $2 $1
+fi
+}
+
+find_null() {
+cat $1 | grep -v ^# | while read line
+do
+	key=$(echo $line | awk -F = '{print $1}')
+	if [ -n "$key" ]
+	then
+		value1=$(echo $line | awk -F = '{print $2}')
+		# 查找$2中对应行
+		./grepi -q -p "^$key=" $2
+		if [ $? -ne 0 ]
+		then # 在$1中存在，在$2中不存在的配置项
+			((sum++))
+			echo "[$key]  <NULL> $value1 $sum"
+		fi
+	fi	
+done
+}
+
 # 必选参数：两个内核配置文件
 # 默认找相同，-v找不同
 common=y
@@ -11,29 +72,12 @@ do
 	esac
 done
 shift $((OPTIND-1))
-cat $1 | grep -v ^# | while read line
-do
-	key=$(echo $line | awk -F = '{print $1}')
-	value1=$(echo $line | awk -F = '{print $2}')
-	# 查找$2中对应行
-	./grepi -q -p ^$key= $2
-	if [ $? -eq 0 ]
-	then
-		# 获取$2中对应行的值
-		value2=$(grep ^$key= $2 | awk -F = '{print $2}')
-		if [ $common = y ]
-		then
-			if [ "$value1" = "$value2" ]
-			then
-				echo $key IN COMMON
-			fi
-		else
-			if [ "$value1" != "$value2" ]
-			then
-				((sum++))
-				echo "[$key]  $value1 $value2 $sum"
-			fi
-		fi
-	fi
-done
-echo "******************************************"
+file1="$1"
+file2="$2"
+if [ -z "$1" ] || [ -z "$2" ]
+then
+	echo "Wrong params"
+	exit 1
+fi
+echo "config | $file1 | $file2 | sum"
+find_include_null $file1 $file2 # 找到$file1中有、$file2中没有的配置项以及$file1和$file2都有、但是值不同的配置项
